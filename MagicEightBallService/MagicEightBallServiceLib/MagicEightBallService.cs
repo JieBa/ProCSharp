e90e584ae7ddbba488d1fe4f23b1ef0e7b258bac
Chao <ringeight@outlook.com>
2016-08-18 22:45:08 +0800

    第一个WCF服务库

---
+++ MagicEightBallService/MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,23 @@
+﻿// 主要的WCF命名空间
+
+using System;
+using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService:IEightBall
+    {
+        // 只是为了在宿主上显示
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awaits your question...");
+        }
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers = {"", "Yes", "No", "Hazy", "Ask agin later", "Definitely"};
+
+            Random r=new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
