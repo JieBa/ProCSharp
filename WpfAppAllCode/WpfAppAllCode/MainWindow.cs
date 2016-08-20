2fbadb622b7a91b8897dc29a3b95ab98ca26fbd7
Chao <ringeight@outlook.com>
2016-08-20 17:28:46 +0800

    第一个WPF程序

---
+++ WpfAppAllCode/WpfAppAllCode/MainWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppAllCode
+{
+    class MainWindow:Window
+    {
+        private Button _btnExitApp=new Button();
+        public MainWindow(string windowTitle,int height,int width)
+        {
+            // 配置按钮并设置子控件
+            _btnExitApp.Click += (sender, e) =>
+            {
+                if ((bool) Application.Current.Properties["GodMode"])
+                {
+                    MessageBox.Show("骗子");
+                }
+                // 关闭窗体
+                Close();
+            };
+            _btnExitApp.Content = "退出应用";
+            _btnExitApp.Height = 25;
+            _btnExitApp.Width = 100;
+
+            // 将窗体的内容设置为一个按钮
+            Content = _btnExitApp;
+
+            // 配置窗体
+            Title = windowTitle;
+            Height = height;
+            Width = width;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Closing += (sender, e) =>
+            {
+                string msg = "你想要在不保存的情况下关闭窗体吗？";
+                MessageBoxResult result = MessageBox.Show(msg, "我的应用", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            };
+            Closed += (sender, e) =>
+            {
+                MessageBox.Show("再见！");
+            };
+
+            // 拦截鼠标事件
+            MouseMove += (sender, e) =>
+            {
+                Title = e.GetPosition(this).ToString();
+            };
+            KeyDown += (sender, e) =>
+            {
+                _btnExitApp.Content=e.Key.ToString();
+            };
+
+        }
+    }
+}
---
+++ WpfAppAllCode/WpfAppAllCode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppAllCode
+{
+    class Program:Application
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Program app=new Program();
+            app.Startup += (sender, e) =>
+            {
+                //Window mainWindow=new Window();
+                //mainWindow.Title = "我的第一个WPC应用";
+                //mainWindow.Height = 200;
+                //mainWindow.Width = 300;
+                //mainWindow.WindowStartupLocation=WindowStartupLocation.CenterScreen;
+                //mainWindow.Show();
+                Application.Current.Properties["GodMode"] = false;
+                foreach (string arg in e.Args)
+                {
+                    if (arg.ToLower() == "/godmode")
+                    {
+                        Current.Properties["GodMode"] = true;
+                        break;
+                    }
+                }
+                MainWindow wnd=new MainWindow("我的第一个WPC应用", 200,300);
+                wnd.Show();
+            };
+
+            app.Exit += (sender, e) =>
+            {
+                MessageBox.Show("应用以退出");
+            };
+            app.Run();
+        }
+    }
+}
